48c711e1347bb25587723c90f4f31e85726506c3
Bryan Weber <bryan.w.weber@gmail.com>
2021-05-23 00:08:30 -0400

    Upload 2021 cleaned notebooks

--- ncm-2021/convert.py
+++ ncm-2021/convert.py
@@ -30,7 +30,10 @@ if __name__ == "__main__":
     HERE = Path(__file__).parent
     output_directory = HERE.joinpath("output")
 
-    for notebook in HERE.glob("*.ipynb"):
+    for notebook in HERE.glob("**/*.ipynb"):
+        if "checkpoints" in str(notebook):
+            continue
+        print(notebook)
         output_notebook = notebook.with_stem(notebook.stem + "-clean").name
         nb = nbformat.read(notebook, as_version=4)
         nb_new = upgrade(nb)
@@ -40,4 +43,4 @@ if __name__ == "__main__":
             del nb.metadata["celltoolbar"]
 
         clean_nb, _ = nb_exp.from_notebook_node(nb)
-        nbformat.write(nbformat.reads(clean_nb, as_version=4), output_directory.joinpath(output_notebook))
+        nbformat.write(nbformat.reads(clean_nb, as_version=4), notebook) 
